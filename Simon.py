a5e96a53597060d9d0cf60f502de98498dbebb24
root <re@localhost.localdomain>
2019-04-22 17:08:33 -0400

    small improvements for efficiency

--- Simon.py
+++ Simon.py
@@ -4,27 +4,27 @@ from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import execute
 import random
 
-num_qubits = 4
+regs = 2
 
-q = QuantumRegister(num_qubits) # 4 qubit register
-c = ClassicalRegister(int(num_qubits/2)) # 1 classical register
+q = QuantumRegister(regs*2) # qubit register
+c = ClassicalRegister(regs) # classical register
 circ = QuantumCircuit(q, c) # Quantum Circuit on q
 
 #generating random secret
 secret = ""
-for i in range(0, int(num_qubits/2)):
-    secret += str(random.randint(0, 10000)%2) #binary string
+for i in range(0, regs):
+    secret += str(random.randint(0, 9)%2) #binary string
 
-print(">> secret is:"+secret+"\n")
+print(">> secret is: "+secret+"\n")
 
-for i in range(0, int(num_qubits/2)):
+for i in range(0, regs):
     circ.h(q[i])
     #print("Hadamard on q[%d]" % i)
 
 #begining of blackbox
 circ.barrier()
-for i in range(0, int(num_qubits/2)):
-    circ.cx(q[i], q[int(i+num_qubits/2)])
+for i in range(0, regs):
+    circ.cx(q[i], q[i+regs])
     #copying first half of registers to second half
 
 print(circ)
