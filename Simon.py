c422c3a96fd1c4e6bed01cfefccaa62b34981c84
root <re@localhost.localdomain>
2019-04-22 16:43:19 -0400

    Beginning of blackbox, copy first part of registers to second pard

--- Simon.py
+++ Simon.py
@@ -10,16 +10,24 @@ q = QuantumRegister(num_qubits) # 4 qubit register
 c = ClassicalRegister(1) # 1 classical register
 circ = QuantumCircuit(q, c) # Quantum Circuit on q
 
-for i in range(0, int(num_qubits/2)):
-    circ.h(q[i])
-    #print("Hadamard on q[%d]" % i)
-print(circ)
-
 #generating random secret
 secret = ""
 for i in range(0, int(num_qubits/2)):
     secret += str(random.randint(0, 10000)%2) #binary string
 
-print(secret)
+print(">> secret is:"+secret+"\n")
+
+for i in range(0, int(num_qubits/2)):
+    circ.h(q[i])
+    #print("Hadamard on q[%d]" % i)
+
+#begining of blackbox
+circ.barrier()
+for i in range(0, int(num_qubits/2)):
+    circ.cx(q[i], q[int(i+num_qubits/2)])
+    #copying first half of registers to second half
+
+print(circ)
+
 
 
