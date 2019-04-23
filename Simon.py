9d5c460d80ff7ee34fe89285909d219f60e6935d
root <re@localhost.localdomain>
2019-04-23 07:29:11 -0400

    Fixed some issues with binary representation of secret

--- Simon.py
+++ Simon.py
@@ -3,19 +3,42 @@ import numpy as np
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import execute
 import random
-
+    
 regs = 2
 
 q = QuantumRegister(regs*2) # qubit register
 c = ClassicalRegister(regs) # classical register
 circ = QuantumCircuit(q, c) # Quantum Circuit on q
+secret = None
+number = None
 
+    
+def gen_rand():
+    number = 0
+    secret = [None] * regs
+    j = regs-1
+    for i in range(0, regs):
+        rand = random.randint(0, 9)%2
+        number += rand*(2**i)
+        print("rand is %d" %rand)
+        secret[j] = str(rand) #binary string
+        j -= 1
+    if number == 0:
+        secret = []
+        j = regs-1
+        return gen_rand()
+    else:
+        return number, secret
+    
 #generating random secret
-secret = ""
-for i in range(0, regs):
-    secret += str(random.randint(0, 9)%2) #binary string
 
-print(">> secret is: "+secret+"\n")
+
+number, secret = gen_rand()
+print("number is %d" %number)
+for i in range(0, len(secret)):
+    print(">> secret[%d] is %s" %(i,secret[i]))
+#print("secret[0] is %c" %secret[0])
+print(">> secret is: "+''.join(secret)+"\n")
 
 for i in range(0, regs):
     circ.h(q[i])
@@ -27,7 +50,17 @@ for i in range(0, regs):
     circ.cx(q[i], q[i+regs])
     #copying first half of registers to second half
 
+for i in range(0, regs):
+    #if i % 2 == 0:
+     #   circ.x(q[i])
+    #else:
+     #   circ.cx(q[i-1], q[i])
+    0
+circ.barrier()
 print(circ)
+
+
+
 
 
 
