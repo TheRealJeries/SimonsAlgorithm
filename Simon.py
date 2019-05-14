25baf6a1a36594a8d5dcb8063738c96c22c274b7
BitmanDuran <eduransa@umd.edu>
2019-05-13 21:32:49 -0400

    adding code to Simon.py

--- Simon.py
+++ Simon.py
@@ -1,5 +1,6 @@
 # imports
 import numpy as np
+from sympy import *
 #import qiskit_aqua.components.oracles 
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import execute
@@ -10,7 +11,104 @@ import matplotlib.pyplot
 from qiskit.tools.visualization import plot_state_city, plot_histogram
 import pylab
 import random
+
+
+#first ipythin cell
+def simons_circuit(qbits, secret):
+    n = qbits
+
+    qr = QuantumRegister(n*2) # 4 qubit register
+    cr = ClassicalRegister(n) # 1 classical register
+    circ = QuantumCircuit(qr, cr) # Quantum Circuit on q
+
+    for i in range(0, n):
+        circ.h(qr[i])
+
+    circ.barrier()
+
+    for i in range(0, n):
+        circ.cx(qr[i], qr[n+i])
+    circ.barrier()
+
+    s = secret
+
+    #xor s.
+    for i, c in enumerate(s):
+        if c == '0':
+            circ.cx(qr[i], qr[n+i])
+
+    circ.barrier()
+
+    for i in range(0, n):
+        circ.h(qr[i])
+        circ.measure(qr[i], cr[i])
+    return circ
+
+#second Ipython cell
+n = 6
+secret = '100011'
+circ = simons_circuit(n, secret)
+#to draw in Ipython notebook
+#circ.draw(output='mpl')
+
+#Third Ipython cell
+simulator = Aer.get_backend('qasm_simulator')
+
+#get first measurement to start making the matrix ofr the system of equations.
+result = execute(circ, simulator, shots=1).result()
+temp = result.get_counts(circ)
+mes = next(iter(temp))
+count = {}
+count[mes] = 1
+wi = get_wi(mes)
+M = Matrix([wi])
+
+#while loop to collect the rest
+dim = 1
+i = 0
+while(True and i < 100 ):
+    #run simul, get measurement
+    result = execute(circ, simulator, shots=1).result()
+    temp = result.get_counts(circ)
+    mes = next(iter(temp))
+    wi = get_wi(mes)
+    if mes in count:
+        count[mes] = count[mes] + 1
+    # make augment matrix by adding the new row
+    ma = M.col_join(Matrix([wi]))
     
+    #check if new measuremnt is in the span of {w_i}
+    m, mr = M.rref()
+    mA, mar = ma.rref()
+    if mr != mar:
+        count[mes] = 1
+        M = ma
+        dim = dim + 1
+    elif (dim == n - 1):
+        break
+    i = i + 1
+    
+#ptrint the system of equations that satisfy s
+rows, cols = M.shape
+for r in range(rows):
+    Yr = [ "s[ "+str(i)+" ]" for i, v in enumerate(list(M[r,:])) if v == 1 ]
+    if len(Yr) > 0:
+        tStr = " + ".join(Yr)
+        print(tStr, "= 0")
+M
+
+#fifth Ipython cell and code to print histogram in notebook
+#plot_histogram(count, title='Simons problem')
+
+# turn the measured string into an array of integers.
+def get_wi(w):
+    v = []
+    for i in range(n):
+        v.append(int(w[(n-i) - 1]))
+    return v
+
+''' old code below
+
 regs = 2
 
 q = QuantumRegister(regs*2) # qubit register
@@ -91,7 +189,7 @@ if simulate:
     #plot_state_city(outputstate)
     #pylab.show()
 else:
-    print("here")
+    print("here")'''
         
 
 
